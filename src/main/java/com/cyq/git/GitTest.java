7c6716a0075a95c63eca73671058c3825f40e216
yq <yq@git.com>
2023-03-14 11:28:08 +0800

    first commit

---
+++ src/main/java/com/cyq/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.cyq.git;
+
+/**
+ * @author cyq
+ * @create 2023-03-14-11:23
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
